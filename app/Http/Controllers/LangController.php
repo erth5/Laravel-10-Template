3591b9d713330176f9e189ff2b679415824c08d6
Eggo5 <Rthem@gmx.de>
2023-03-17 23:39:08 +0100

    Controller for Lang CRUD

---
+++ app/Http/Controllers/LangController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lang;
+use App\Http\Requests\StoreLangRequest;
+use App\Http\Requests\UpdateLangRequest;
+
+class LangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lang $lang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lang $lang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLangRequest $request, Lang $lang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lang $lang)
+    {
+        //
+    }
+}
