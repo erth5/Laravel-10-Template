b4014ad6084c653a237d0732a0cf96e4aad89a80
Eggo5 <Rthem@gmx.de>
2023-04-19 20:45:17 +0200

    add typescript prepair

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+const content = document.querySelector('#');
+function loadMoreContent(entry) {
+    if (entry.target === content) import('./content.js')
+}
+
+var observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            loadMoreContent(entry);
+        }
+    });
+},
+    {
+        root: null,
+        rootMargin: '0px',
+        thresold: 0.1,
+    }
+);
+
+observer.observe(content);
