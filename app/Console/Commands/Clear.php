6ef8c590cd0ccc80b3a6a5cc21c60f45a6f48f71
ruben.matthes <ruben.matthes@hsc-solutions.de>
2023-11-14 11:07:36 +0100

    TODO: clear commands aufbereiten

--- app/Console/Commands/Clear.php
+++ app/Console/Commands/Clear.php
@@ -29,10 +29,25 @@ class Clear extends Command
         }
 
         if(config('on_windows'))
+
+
+        
         try{
             // rm -rf ~/.composer/cache - rmdir /s /q %USERPROFILE%\AppData\Roaming\Composer\Cache
         }catch(\Exception $e){
             
+        }
+    }
+    public function clearComposerCache() {
+        $command = 'rmdir /s /q %USERPROFILE%\AppData\Roaming\Composer\Cache';
+        exec($command, $output, $returnVar);
+    
+        if ($returnVar !== 0) {
+            // Fehlerbehandlung
+            Log::error("Fehler beim Leeren des Composer-Cache: " . implode("\n", $output));
+        } else {
+            // Erfolgsmeldung
+            Log::info("Composer-Cache erfolgreich geleert.");
         }
     }
 }
